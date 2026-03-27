d396993294f10a0cb4c52ad687d6c8ece0a70071
thanhnn5 <thanh.nguyen@gojitsu.com>
2026-03-27 13:50:11 +0700

    use RGB for DINO pretrained

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 python3 tools/train.py -c configs/det/PP-OCRv5/PP-OCRv5_dinov3_det.yml \
-    -o Train.dataset.data_dir=./ocr_det_dataset_examples \
-    Train.dataset.label_file_list='[./ocr_det_dataset_examples/train.txt]' \
-    Eval.dataset.data_dir=./ocr_det_dataset_examples \
-    Eval.dataset.label_file_list='[./ocr_det_dataset_examples/val.txt]'
+    -o Global.epoch_num=50 \
+    Global.print_batch_step=50 \
+    Train.dataset.loader.batch_size_per_card=8 \
+    Train.dataset.data_dir=data/ocr_det_mask_3-1/ \
+    Train.dataset.label_file_list='[./data/ocr_det_mask_3-1/train.txt]' \
+    Eval.dataset.data_dir=data/ocr_det_mask_3-1/ \
+    Eval.dataset.label_file_list='[./data/ocr_det_mask_3-1/val.txt]'
