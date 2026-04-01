f40c4cdc9e6bd0f0b3f10dd38fd5235a7efadd3c
thanhnn5 <thanh.nguyen@gojitsu.com>
2026-04-01 14:42:07 +0700

    Update script infer

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
-python3 tools/infer_det.py -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det.yml \
-    -o Global.pretrained_model=weights/mob_det_2.pth \
-    Global.infer_img="/Users/thanhnn5/Workspace/Jitsu/POD/PytorchOCR/images" \
-    PostProcess.box_thresh=0.7 \
+python3 tools/infer_det.py -c configs/det/PP-OCRv5/PP-OCRv5_convnext_det.yml \
+    -o Global.pretrained_model=output/PP-OCRv5_convnext_det_unfreeze/best.pth \
+    Global.infer_img="data/ocr_test_20251127/images/" \
+    Global.output_dir="output/predictions" \
+    PostProcess.box_thresh=0.6 \
     Global.limit_side_len=1280 \
     Global.limit_type="max"
